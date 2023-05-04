53e4adbf26b456b46a38c4a33a0caa1125b6094f
Silvio Martins Santos <87779169+SilvioUFERSA@users.noreply.github.com>
2023-05-04 13:35:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,51 +2,62 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
+scene.fog = new THREE.Fog(0x5d0361, 10, 1500);
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+
+const ambientLight = new THREE.HemisphereLight(0x404040, 0x404040, 1);
+
+const directionalLight = new THREE.DirectionalLight(0xffffdd, 1);
+directionalLight.position.set(-300, 0, 600);
+
+const pointLight = new THREE.PointLight(0x131148, 2, 1000, 2);
+pointLight.position.set(200, -100, 50);
+
+scene.add(ambientLight, directionalLight, pointLight);
+
+const renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
+renderer.shadowMap.enabled = true;
+
+let gltf_;
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const loader = new GLTFLoader();
 loader.load(
-	// resource URL
+
 	'./foguete.gltf',
-	// called when the resource is loaded
+
 	function ( gltf ) {
 
 		scene.add( gltf.scene );
-
-		gltf.animations; // Array<THREE.AnimationClip>
-		gltf.scene; // THREE.Group
-		gltf.scenes; // Array<THREE.Group>
-		gltf.cameras; // Array<THREE.Camera>
-		gltf.asset; // Object
+		gltf_ = gltf.scene;
 
 	},
-	// called while loading is progressing
-	function ( xhr ) {
 
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-	},
-	// called when loading has errors
-	function ( error ) {
-
-		console.log( 'An error happened' );
-
-	}
 );
-camera.position.z = 10;
+camera.position.z = 60;
+camera.position.y=-10
+camera.position.x=0
+
+const targetRocketPosition = 8;
+const animationDuration = 5000;
+
 
 function animate() {
-	requestAnimationFrame( animate );
+	
+	const t = (Date.now() % animationDuration) / animationDuration;
+	
 	renderer.render( scene, camera );
-	if (gltf) {
-        gltf.scene.rotation.x += 0.01;
-        gltf.scene.rotation.y += 0.01;
-        gltf.scene.scale.set(0.5, 0.5, 0.5);
+
+	const delta = targetRocketPosition * Math.sin(Math.PI * 2 * t);
+	if (gltf_) {
+        gltf_.rotation.y += 0.01;   
+		gltf_.position.y = delta; 
     }
+
+	requestAnimationFrame( animate );
 }
 
 animate();
